d4142e726251a2f06be027d273fdd93d0b296fec
Hester G <hester.gent@mayden.co.uk>
2026-09-25 15:07:18 +0100

    Use absolute path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
+
+const __dirname = import.meta.dirname
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@t': 'src/types',
-      '@shared': 'src/components/shared',
+      '@t': path.resolve(__dirname, 'src/types'),
+      '@shared': path.resolve(__dirname, 'src/components/shared'),
     },
   },
 })
